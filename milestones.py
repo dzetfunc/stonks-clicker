c3152130fbc9fe08777a21d55391a9e524dd5184
Daria Musatkina <dzetfunc@gmail.com>
2020-09-13 06:55:58 +0300

    I'm tired, so first part of messages

--- milestones.py
+++ milestones.py
@@ -4,13 +4,17 @@ import os
 
 
 def show_milestone(bot: TeleBot, mess: Message, content: str):
+    photo_exist = false
     if os.path.exists(os.path.join("content", content + ".png")):
         photo_path = os.path.join("content", content + ".png")
-    else:
+        photo_exist = true
+    if os.path.exists(os.path.join("content", content + ".jpg")):
         photo_path = os.path.join("content", content + ".jpg")
+        photo_exist = true
 
     with open(os.path.join("content", content + ".txt")) as f:
         caption = f.read()
 
-    with open(photo_path, 'rb') as f:
-        bot.send_photo(mess.chat.id, photo=f, caption=caption)
+    if photo_exist:
+        with open(photo_path, 'rb') as f:
+            bot.send_photo(mess.chat.id, photo=f, caption=caption)
